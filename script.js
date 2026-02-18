207f848aada1e85fb8b64cad714d072ffcfa9b72
Orion Stellaris <jonhfeel88@gmail.com>
2026-02-18 14:19:55 +0400

    input plus disabled y/n

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 // Select elements
-let butt = document.querySelector('#b');
-let element =document.querySelector('#e');
+const butt = document.querySelector('#b');
+const element =document.querySelector('#e');
+
+// for disabled input
+const contact = document.querySelector("#contact");
+const send = document.querySelector("#send");
+contact.disabled = false;
+
+// Disabled input false or true
+send.addEventListener("click", function() {
+    if (contact.disabled) {
+        contact.disabled = false;
+        console.log("Input: false");
+    } else {
+        contact.disabled = true;
+        console.log("Input: true");
+    }
+})
 
 // add click event listener to the button to toggle the visibility of the information block
 if (butt && element) {
     butt.addEventListener('click', () => {
     element.classList.toggle('active');
     console.log('Button clicked, toggling information block visibility');
-
+    
     if (element.classList.contains('active')) {
         console.log('Information block is now visible');
     } else {
