a4888200937a9e315185d5d49eace2ec5ddbc8fa
Orion Stellaris <jonhfeel88@gmail.com>
2026-02-18 16:28:44 +0400

    fix

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 // Select elements
 const butt = document.querySelector('#b');
 const element =document.querySelector('#e');
@@ -14,22 +16,24 @@ send.addEventListener("click", () => {
     if (contact.disabled) {
         contact.disabled = false;
         console.log("Input: false");
+        contact.focus();
     } else {
         contact.disabled = true;
         console.log("Input: true");
+        contact.blur();
     }
-})
+});
 
 // add click event listener to the button to toggle the visibility of the information block
 if (butt && element) {
     butt.addEventListener('click', () => {
     element.classList.toggle('active');
-    console.log('Button clicked, toggling information block visibility');
+    console.log('Button clicked, toggling information block: visibility');
     
     if (element.classList.contains('active')) {
-        console.log('Information block is now visible');
+        console.log('Information block is now: visible');
     } else {
-        console.log('Information block is now hidden');
+        console.log('Information block is now: hidden');
     }
 });
 };
